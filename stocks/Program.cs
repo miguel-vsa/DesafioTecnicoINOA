b10ace6fe4a95998cd5e94fc3ed5695aa75271a4
AlphaOrionis20 <angelomvieira20@gmail.com>
2026-09-23 18:36:32 -0300

    comand-line

--- stocks/Program.cs
+++ stocks/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Uso: stock-quote-alert <ativo> <preço-venda> <preço-compra>");
+            return;
+        }
+
+        string ativo = args[0];
+
+        Console.WriteLine($"Ativo: {ativo}");
+    }
+}
